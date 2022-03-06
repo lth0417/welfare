49b457c4ef72ec3b6393abeb9b4bed345d4035c1
lth0417 <1689591831@qq.com>
2022-03-06 16:52:42 +0800

    用户管理的部分已更新

--- src/main/java/cn/com/sise/welfare/controller/PageController.java
+++ src/main/java/cn/com/sise/welfare/controller/PageController.java
@@ -15,34 +15,54 @@ public class PageController {
     @Value("${virtual.file.path}")
     private String path;
 
+    /**
+     * 跳转登录页面
+     * @return
+     */
     @GetMapping("/login")
     public String login(){
         return "login";
     }
 
+    /**
+     * 退出
+     * @return
+     */
     @GetMapping("/logout")
     public String logout(){
         SecurityUtils.getSubject().logout();
         return "redirect:/login";
     }
 
+    /**
+     * 跳转到admin
+     * @return
+     */
     @GetMapping("/admin")
     public String toAdmin(){
         return "/backed/admin/admin";
     }
 
+    /**
+     * 跳转到注册页面
+     * @return
+     */
     @GetMapping("/register")
     public String toRegister(){
         return "/register";
     }
 
+    /**
+     * 跳转到我的桌面
+     * @return
+     */
     @GetMapping("/welcome")
     public String toWelcome(){
         return "/welcome";
     }
 
     /**
-     * 跳转到白名单
+     * 跳转到黑名单
      * @return
      */
     @GetMapping("/toBlackList")
@@ -51,10 +71,20 @@ public class PageController {
         return "/backed/admin/blackList";
     }
 
+    /**
+     * 跳转到白名单
+     * @param model
+     * @return
+     */
     @GetMapping("/toWhiteList")
     public String toWhiteList(Model model) {
         model.addAttribute("filePath", path);
         return "/backed/admin/whiteList";
     }
 
+    //跳转到组织管理列表
+    @GetMapping("/toAuditList")
+    public String toAuditList(){
+        return "/backed/admin/toAuditList";
+    }
 }
