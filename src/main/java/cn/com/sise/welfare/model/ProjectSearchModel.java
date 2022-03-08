de41d7c01996ba8e6daeb34202e10b95393c4950
linsia520 <1187698878@qq.com>
2022-03-08 14:21:58 +0800

    完善细节

--- src/main/java/cn/com/sise/welfare/model/ProjectSearchModel.java
+++ src/main/java/cn/com/sise/welfare/model/ProjectSearchModel.java
@@ -1,9 +1,10 @@
 package cn.com.sise.welfare.model;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Data;
 
 @Data
-public class ProjectSearchModel {
+public class ProjectSearchModel extends Page<ProjectSearchModel> {
     private String id;
     private String title;
     private String content;
